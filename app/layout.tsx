5e4e75286959767a4ce370c3c2adb4c0fca7e52a
oliverjam <oliverjamesphillips@gmail.com>
2024-11-04 17:29:23 +0000

    Remove links from navbar for now

--- app/layout.tsx
+++ app/layout.tsx
@@ -25,16 +25,16 @@ export default function RootLayout({
 	return (
 		<html lang="en">
 			<body className={`${mona.className} text-slate-dark`}>
-				<nav className="ScrollShrink max-w-7xl z-50 rounded-2xl p-2 sm:p-4 text-sm sm:text-base bg-white/90 shadow-lg backdrop-blur-[2px] flex items-center gap-4 md:gap-8 lg:gap-16 fixed top-8 left-2 right-2 mx-auto">
+				<nav className="ScrollShrink max-w-7xl z-50 rounded-2xl p-2 sm:p-4 text-sm sm:text-base bg-white/90 shadow-lg backdrop-blur-[2px] flex items-center justify-between gap-4 md:gap-8 lg:gap-16 fixed top-8 left-2 right-2 mx-auto">
 					<Img file={logo} alt="Supply25" className="w-24 sm:w-32 md:w-40" />
-					<div className="flex gap-4 md:gap-8 flex-1 font-medium text-slate-dark">
+					{/* <div className="flex gap-4 md:gap-8 flex-1 font-medium text-slate-dark">
 						<Link href="/" className="hover:underline">
 							Home
 						</Link>
 						<Link href="/team" className="hover:underline">
 							Team
 						</Link>
-					</div>
+					</div> */}
 					<CallToAction />
 				</nav>
 				{children}
