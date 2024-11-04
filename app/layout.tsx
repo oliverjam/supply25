fe6f288ef9aa9330c44dd912365b28e1491a05b7
oliverjam <oliverjamesphillips@gmail.com>
2024-11-04 18:13:01 +0000

    Add favicons

--- app/layout.tsx
+++ app/layout.tsx
@@ -24,6 +24,24 @@ export default function RootLayout({
 }>) {
 	return (
 		<html lang="en">
+			<head>
+				<title>Supply25</title>
+				<link
+					rel="icon"
+					type="image/png"
+					href="/favicon-96x96.png"
+					sizes="96x96"
+				/>
+				<link rel="icon" type="image/svg+xml" href="/favicon.svg" />
+				<link rel="shortcut icon" href="/favicon.ico" />
+				<link
+					rel="apple-touch-icon"
+					sizes="180x180"
+					href="/apple-touch-icon.png"
+				/>
+				<meta name="apple-mobile-web-app-title" content="Supply25" />
+				<link rel="manifest" href="/site.webmanifest" />
+			</head>
 			<body className={`${mona.className} text-slate-dark`}>
 				<nav className="ScrollShrink max-w-7xl z-50 rounded-2xl p-2 sm:p-4 text-sm sm:text-base bg-white/90 shadow-lg backdrop-blur-[2px] flex items-center justify-between gap-4 md:gap-8 lg:gap-16 fixed top-8 left-2 right-2 mx-auto">
 					<Img file={logo} alt="Supply25" className="w-24 sm:w-32 md:w-40" />
