1a15875dae641054083dca6b54038cbaa252f95a
oliverjam <oliverjamesphillips@gmail.com>
2024-11-04 17:33:43 +0000

    Add calendar link to CTA

--- app/cta.tsx
+++ app/cta.tsx
@@ -2,7 +2,11 @@ import { Phone } from "./icons.tsx";
 
 export function CallToAction() {
 	return (
-		<a className="max-w-max flex items-center gap-2 rounded shadow p-2 sm:p-3 bg-pink text-white font-bold">
+		<a
+			href="https://calendar.app.google/ypJiHFiobzbyidXJ6"
+			target="_blank"
+			className="max-w-max flex items-center gap-2 rounded shadow p-2 sm:p-3 bg-pink text-white font-bold"
+		>
 			<Phone size={24} />
 			Book a Call
 		</a>
